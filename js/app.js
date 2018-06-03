76ff92edb98e37bba216b99a59dd58a41683e270
Naji <najielarifi@gmail.com>
2018-06-03 17:12:01 +0100

    made changes to logic so that when clicked images are reveleved
    sldier also changes the color of the background
    8 images added so that the images for the game are there
    basic styling done
    working on logic next for card flip and completion

--- js/app.js
+++ js/app.js
@@ -7,17 +7,42 @@ const eachRow = document.getElementById("table-square");
 
 
 //When a use clicks then this calls the desired function
+
 eachRow.addEventListener('click', showCard);
 
 
 function showCard(event) {
-	console.log(event.target.src);
-	if (event.target.src === "file:///Users/nelarifi/Dropbox/Courses/udacity/memory-game/img/square.png") {
-		event.target.src = "img/squaremouse.png"
+	const clickedImage = event.target.src;
+	const theImage = clickedImage.endsWith("square.png");
+	const classClicked = event.target.className
+	console.log(clickedImage)
+	console.log(classClicked);
+	if (theImage === true) {
+		imageShower(clickedImage, classClicked);
 	} else {
-		event.target.src = "img/square.png"
+		event.target.src = "img/square.png";	
 	}
 	
+}
+
+// This function below takes 2 arguments from the showCard function and then shows and hides the image that are clicked on.
+
+function imageShower(clickedImage, classClicked) {
+	if (classClicked == "image-one square-down") {
+			event.target.src = "img/icons/one.png";
+		} else if (classClicked == "image-two square-down") {
+			event.target.src = "img/icons/two.png";
+		}else if (classClicked == "image-three square-down") {
+			event.target.src = "img/icons/three.png";
+		}else if (classClicked == "image-four square-down") {
+			event.target.src = "img/icons/four.png";
+		}else if (classClicked == "image-five square-down") {
+			event.target.src = "img/icons/five.png";
+		}else if (classClicked == "image-six square-down") {
+			event.target.src = "img/icons/six.png";
+		}else if (classClicked == "image-seven square-down") {
+			event.target.src = "img/icons/seven.png";
+		}else event.target.src = "img/icons/eight.png";
 }
 
 
@@ -27,25 +52,25 @@ function showCard(event) {
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+slider.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-    output.innerHTML = this.value;
+    slider.innerHTML = this.value;
     theValue = this.value
     var cubeColor = document.getElementsByClassName("square-down")
-    if (theValue <= 50 && theValue > 25) {
+    if (theValue == 2) {
     	// console.log("less");
     	var i;
     	for (i = 0; i < cubeColor.length; i++) {
     		cubeColor[i].style.backgroundColor = "#D1CE87";
 		}
-    } else if (theValue <= 25){
+    } else if (theValue == 1){
     	// console.log("more");
     	for (i = 0; i < cubeColor.length; i++) {
     		cubeColor[i].style.backgroundColor = "#D19987";
 		}
-    } else if (theValue >= 50 && theValue < 75){
+    } else if (theValue == 3){
     	for (i = 0; i < cubeColor.length; i++) {
     		cubeColor[i].style.backgroundColor = "#93D187";
 		}
