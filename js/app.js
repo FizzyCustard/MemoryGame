808d44417182d0947eaf71794a6623650f53eb88
FizzyCustard <4170256+FizzyCustard@users.noreply.github.com>
2018-06-21 09:16:43 +0100

    Basic timer added and starts and stops when game begins and when user wins
    TODO stoped using the alert as it was causing issue with image load so need to add the module pop up.
    TODO need to work on randomising the images next

--- js/app.js
+++ js/app.js
@@ -1,61 +1,123 @@
-
-
-
-
-const eachCube = document.getElementsByClassName("square-down") ;
+const eachCube = document.getElementsByClassName("square-down");
 const eachRow = document.getElementById("table-square");
 const trackScore = document.getElementById("userScore");
 const feedback = document.getElementById("userFeedback");
 
 eachRow.addEventListener('click', showCard);
-var previousClicked = undefined;
-var imageClicked = undefined;
-var score = 0
-var theImage;
-var clickTarget;
-var previousClickedEvent;
-var flippedCards = [];
-var thing;
+let previousClicked = undefined;
+let imageClicked = undefined;
+let score = 0
+let theImage;
+let clickTarget;
+let previousClickedEvent;
+let flippedCards = [];
+let thing;
+
+
+//code for the timer
+
+let seconds = 00;
+let tens = 00;
+const appendTens = document.getElementById("tens")
+const appendSeconds = document.getElementById("seconds")
+const buttonStart = document.getElementById('button-start');
+const buttonStop = document.getElementById('button-stop');
+const buttonReset = document.getElementById('button-reset');
+let Interval;
+let timerCheck = 0
+
+// buttonStart.onclick = function() {
+
+//      clearInterval(Interval);
+//      Interval = setInterval(startTimer, 10);
+//   }
+
+function timerStop() {
+    clearInterval(Interval);
+}
+
+function timerReset() {
+    clearInterval(Interval);
+    tens = "00";
+    seconds = "00";
+    appendTens.innerHTML = tens;
+    appendSeconds.innerHTML = seconds;
+}
+
+
+function startTimer() {
+    tens++;
+    if (tens < 9) {
+        appendTens.innerHTML = "0" + tens;
+    }
+    if (tens > 9) {
+        appendTens.innerHTML = tens;
+    }
+
+    if (tens > 99) {
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+    }
+
+    if (seconds > 9) {
+        appendSeconds.innerHTML = seconds;
+    }
+
+}
+
+
+//TIMER CODE END
+
 
 function showCard(event) {
-	feedback.innerHTML = "";
-	flippedCards.push(event);
-	clickTarget = event;
-	clickedImage = clickTarget.target.src;
-	theImage = clickedImage.endsWith("square.png");
-	imageClicked = clickTarget.target.className;
-	
-	if (theImage === true) {
-		imageShower(imageClicked);
+    feedback.innerHTML = "";
+    flippedCards.push(event);
+    clickTarget = event;
+    clickedImage = clickTarget.target.src;
+    theImage = clickedImage.endsWith("square.png");
+    imageClicked = clickTarget.target.className;
 
-		compareImage();
-		} else {
-		console.log("You can not double click image");
-	}
-	previousClickedEvent = clickTarget;
-	winCheck();
+    if (theImage === true) {
+        imageShower(imageClicked);
 
+        compareImage();
+    } else {
+        console.log("You can not double click image");
+    }
+    previousClickedEvent = clickTarget;
+
+
+    if (timerCheck === 0) {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+        console.log("Starting timer")
+        timerCheck++
+    } else console.log("Not starting timer")
+
+    winCheck();
 }
 
 
 // This function below takes 2 arguments from the showCard function and then shows and hides the image that are clicked on.
 
 function imageShower(imageClicked) {
-	if (imageClicked == "image-one square-down") {
-			clickTarget.target.src = "img/icons/one.png";
-		} else if (imageClicked == "image-two square-down") {
-			clickTarget.target.src = "img/icons/two.png";
-		}else if (imageClicked == "image-three square-down") {
-			clickTarget.target.src = "img/icons/three.png";
-		}else if (imageClicked == "image-four square-down") {
-			clickTarget.target.src = "img/icons/four.png";
-		}else if (imageClicked == "image-five square-down") {
-			clickTarget.target.src = "img/icons/five.png";
-		}else if (imageClicked == "image-six square-down") {
-			clickTarget.target.src = "img/icons/six.png";
-		}else if (imageClicked == "image-seven square-down") {
-			clickTarget.target.src = "img/icons/seven.png";
-		}else clickTarget.target.src = "img/icons/eight.png";
+    if (imageClicked == "image-one square-down") {
+        clickTarget.target.src = "img/icons/one.png";
+    } else if (imageClicked == "image-two square-down") {
+        clickTarget.target.src = "img/icons/two.png";
+    } else if (imageClicked == "image-three square-down") {
+        clickTarget.target.src = "img/icons/three.png";
+    } else if (imageClicked == "image-four square-down") {
+        clickTarget.target.src = "img/icons/four.png";
+    } else if (imageClicked == "image-five square-down") {
+        clickTarget.target.src = "img/icons/five.png";
+    } else if (imageClicked == "image-six square-down") {
+        clickTarget.target.src = "img/icons/six.png";
+    } else if (imageClicked == "image-seven square-down") {
+        clickTarget.target.src = "img/icons/seven.png";
+    } else clickTarget.target.src = "img/icons/eight.png";
 }
 
 
@@ -63,35 +125,35 @@ function imageShower(imageClicked) {
 // compares it to the one just clicked to see if they match
 
 function compareImage() {
-	if (previousClicked == undefined) {
-		console.log("Nothing previous");
-		previousClicked = imageClicked;
-	} else if (previousClicked == imageClicked) {
-		console.log("Hooray!!");
-		addPoint();
-	} else if (previousClicked != imageClicked) {
-		console.log("boooooo!!") ;
-		userMistake();
-		feedback.innerHTML = "Woops you made a bo bo!";
-	} else console.log("ERROR");
-		
+    if (previousClicked == undefined) {
+        console.log("Nothing previous");
+        previousClicked = imageClicked;
+    } else if (previousClicked == imageClicked) {
+        console.log("Hooray!!");
+        addPoint();
+    } else if (previousClicked != imageClicked) {
+        console.log("boooooo!!");
+        userMistake();
+        feedback.innerHTML = "Woops you made a bo bo!";
+    } else console.log("ERROR");
+
 }
 
 //Function runs when a user clicks 2 wrong cards
 function userMistake() {
-	thing = previousClickedEvent; //this is for the delayed flip funtion to run
-		setTimeout(turnReset, 800);
-		clearClicked();
-		removeLastFlipped();
-		addPoint();
+    thing = previousClickedEvent; //this is for the delayed flip funtion to run
+    setTimeout(turnReset, 800);
+    clearClicked();
+    removeLastFlipped();
+    addPoint();
 }
 
 //FOR WHEN THE USER SELECTS 2 CORRENT CARDS
 function addPoint() {
-	score = score + 1;
-	trackScore.innerHTML = score;
-	feedback.innerHTML = "Awesome!";
-	clearClicked();
+    score = score + 1;
+    trackScore.innerHTML = score;
+    feedback.innerHTML = "Awesome!";
+    clearClicked();
 
 
 }
@@ -99,49 +161,52 @@ function addPoint() {
 
 //MAKES A PREVIOUS CLICKED AND CURRENT CLICKED CARDS TURN BACK OVER
 function turnReset() {
-	clickTarget.target.src = "img/square.png";
-	thing.target.src = "img/square.png";
-	feedback.innerHTML = "";
-	
+    clickTarget.target.src = "img/square.png";
+    thing.target.src = "img/square.png";
+    feedback.innerHTML = "";
+
 }
 
 
 //THIS CLEARS THE VARAIBLES USED AFTER EACH MOVE OF 2
 function clearClicked() {
-		previousClicked = undefined ;
-		imageClicked = undefined ;
+    previousClicked = undefined;
+    imageClicked = undefined;
 }
 
 
 //REMOVES LAST 2 ITEMS FROM THE FLIPPED ARRAY
-function removeLastFlipped(){
-	flippedCards.pop();
-	flippedCards.pop();
+function removeLastFlipped() {
+    flippedCards.pop();
+    flippedCards.pop();
 }
 
 
 //LOOP THAT FLIPS ALL CARDS BACK OVER TO RESET GAME
 function gameReset() {
-	for (var i = flippedCards.length - 1; i >= 0; i--) {
-		flippedCards[i].target.src = "img/square.png";
-	}
-	score = 0;
-	trackScore.innerHTML = score;
+    for (let i = flippedCards.length - 1; i >= 0; i--) {
+        flippedCards[i].target.src = "img/square.png";
+    }
+    score = 0;
+    trackScore.innerHTML = score;
 }
 
 
 //FUNCTION THAT CHECKS IS THE USER HAS WON
 function winCheck() {
-	if (flippedCards.length == 16) {
-		console.log("WINNER");
-	} 
+    if (flippedCards.length == 16) {
+        console.log("WINNER");
+        timerStop();
+        //TODO: need to fix this little hack
+        feedback.innerHTML = "Great work you won!!! In a time of " + seconds + "." + tens;
+    }
 }
 
 
 // CODE FOR THE SLIDER
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
 slider.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
@@ -149,29 +214,23 @@ slider.oninput = function() {
     slider.innerHTML = this.value;
     theValue = this.value
     if (theValue == 1) {
-    	var i;
-    	for (i = 0; i < eachCube.length; i++) {
-    		eachCube[i].style.backgroundColor = "#ffffff";
-		}
-    } else if (theValue == 2){
-    	for (i = 0; i < eachCube.length; i++) {
-    		eachCube[i].style.backgroundColor = "#DCDC1A";
-		}
-    } else if (theValue == 3){
-    	for (i = 0; i < eachCube.length; i++) {
-    		eachCube[i].style.backgroundColor = "#32D333";
-		}
+        let i;
+        for (i = 0; i < eachCube.length; i++) {
+            eachCube[i].style.backgroundColor = "#ffffff";
+        }
+    } else if (theValue == 2) {
+        for (i = 0; i < eachCube.length; i++) {
+            eachCube[i].style.backgroundColor = "#DCDC1A";
+        }
+    } else if (theValue == 3) {
+        for (i = 0; i < eachCube.length; i++) {
+            eachCube[i].style.backgroundColor = "#32D333";
+        }
     } else {
-    	for (i = 0; i < eachCube.length; i++) {
-    		eachCube[i].style.backgroundColor = "#300B39";
-    	}
-    }   
+        for (i = 0; i < eachCube.length; i++) {
+            eachCube[i].style.backgroundColor = "#300B39";
+        }
+    }
 }
 
 //END OF SLIDER CODE
-
-
-
-
-
-
